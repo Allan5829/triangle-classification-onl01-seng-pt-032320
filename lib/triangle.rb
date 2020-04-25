814d8ca6c540c668b9abe6bf6c6fe1262176814c
Allan Farca <allan.farca@gmail.com>
2020-04-25 05:12:49 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,18 +10,14 @@ class Triangle
   
   def kind 
     #binding.pry
+    raise TriangleError if @side_one <= 0 || @side_two <= 0 || @side_three <= 0
+    raise TriangleError if (@side_one + @side_two <= @side_three) || (@side_three + @side_one <= @side_two) || (@side_two + @side_three <= @side_one)
     if @side_one == @side_two && @side_two == @side_three
       return :equilateral
     elsif @side_three == @side_one || @side_three == @side_two || @side_one == @side_two
       return :isosceles
     elsif @side_one > 0 && @side_two > 0 && @side_three > 0 
       return :scalene
-    elsif @side_one <= 0 || @side_two <= 0 || @side_three <= 0 
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-          puts error.message
-      end
     end 
   end 
   
